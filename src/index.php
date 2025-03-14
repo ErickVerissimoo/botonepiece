ea8f91ddc855e9bc7cc9b62695e052641aab9b45
ErickVerissimoo <erickverissimodasilva144@gmail.com>
2025-03-14 14:43:10 -0300

    mudanças

--- src/index.php
+++ src/index.php
@@ -1,39 +1,50 @@
 <?php
 
-namespace Discord\Proihibida;
+namespace Discord\Proibida;
 
-require_once __DIR__.'/../vendor/autoload.php';
-require_once __DIR__.'/../rb-mysql.php' ;
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/rb-mysql.php';
+require_once __DIR__.'/U.php';
 use Discord\Discord as bot;
 use R;
 
-R::setup('mysql:host=172.18.0.2;dbname=bot', 'root','rootpassword');
+if (!R::testConnection()) {
+    R::setup('mysql:host=172.18.0.2;dbname=bot', 'root', 'rootpassword');
+}
 
 use Discord\Parts\Channel\Message;
+use Discord\Parts\Embed\Embed;
 use Discord\WebSockets\Event;
 use Discord\WebSockets\Intents;
 use \Dotenv\Dotenv as env;
-$en = env::createImmutable(__DIR__);
+
+$en = env::createImmutable(__DIR__ . '/../');
 $en->safeLoad();
 $token = $_ENV['TOKEN'];
+
+// Test database (optional)
 $pessoa = R::dispense('pessoa');
-$pessoa->nome="erick";
+$pessoa->nome = "erick";
 R::store($pessoa);
 
-$discord = new bot(['token' => $token, 'intents' =>[ Intents::GUILD_MESSAGES],    'shardId' => 0,
-'shardCount' => 5] );
-$discord->on('init', function (bot $discord){
-echo 'bot iniciou'.PHP_EOL;
+$discord = new bot([
+    'token' => $token,
+    'intents' => [Intents::GUILD_MESSAGES, Intents::MESSAGE_CONTENT],
+    'shardId' => 0,
+    'shardCount' => 5
+]);
 
+$discord->on('init', function (bot $discord) {
+    echo 'Bot iniciou' . PHP_EOL;
+});
 
+$discord->on(Event::MESSAGE_CREATE, function (Message $message, bot $discord) {
+    if ($message->author->bot) {
+        return;
+    }
+    if ($message->content === "!roll") {
+        $message->channel->sendMessage("<@{$message->author->id}>", false, U::getSomeAkuma($discord));
+    }
 });
-$discord->on(Event::MESSAGE_CREATE, function(Message $message){
-if($message->author->bot){
-return;
-}
-if($message->content==="!teste"){
-$message->reply('teste passado');
-}
-});
+
 $discord->run();
-
--- src/raridades.php
+++ src/raridades.php
@@ -2,33 +2,37 @@
 # PARAMECIAS
 namespace Discord\Proihibida;
 require_once __DIR__.'/../vendor/autoload.php';
-
 use DI\Container;
 
-$comum1='Beri Beri no mi, Shari Shari no mi, Fuku Fuku no mi, Koro Koro no mi, Sube Sube no mi, Iro Iro No Mi, Poke Poke no mi, Kobu Kobu No Mi, Kilo Kilo No Mi, Guru Guru No Mi, Toge Toge no mi, Awa Awa No Mi, Ami Ami No Mi, Iro Iro No Mi, Woshu Woshu No Mi, Sui Sui No Mi, Ori Ori No Mi, Nagi Nagi No Mi, Noko Noko No Mi, Mane Mane No Mi, Kuku Kuku No Mi, Jara Jara No Mi';
-$raro1='Bata Bata no Mi, Bara Bara no Mi, Bane Bane No mi, Kama Kama No Mi, Giro Giro No Mi, Hira Hira No Mi, Suke Suke No Mi, Gocha Gocha No Mi, Mini Mini No Mi, Shibo Shibo No Mi, Beta Beta No Mi, Choki Choki No Mi, Doru Doru No Mi, Noro Noro No Mi, Kuri Kuri No Mi, Memo Memo No Mi, Nui Nui No Mi, Mato Mato No Mi, Modo Modo No Mi, Bomu Bomu No Mi, Jake Jake no mi, Supa Supa No Mi, Goe Goe no Mi, Mosa Mosa No Mi, Sabi Sabi No Mi, Muchi Muchi no Mi, Nomi Nomi no Mi, Hiso Hiso no Mi';
-$epico1='Doa Doa No Mi, Buki Buki No Mi, Kachi Kachi No Mi, Pamu Pamu No Mi, Chiyu Chiyu No Mi, Oto Oto no Mi, Buku Buku No Mi, Bisu Bisu No Mi, Gutsu Gutsu No Mi, Oshi Oshi No Mi, Ton Ton No Mi, Kyubu Kyubu No Mi, Ishi Ishi No Mi, Yomi Yomi No Mi, Mira Mira No Mi, Goru Goru No Mi, Hoya Hoya No Mi, Pero Pero No Mi, Shiro Shiro No Mi, Baku Baku No Mi, Gabu Gabu no Mi';
-$lendario1='Mero Mero No Mi, Wara Wara No Mi, Ato Ato No Mi, Hana Hana No Mi, Hobi Hobi No Mi, Modo Modo no Mi, Nuke Nuke No Mi, Jiki Jiki No Mi, Maki Maki no Mi, Juku Juku No Mi, Kage Kage No Mi, Peto Peto No Mi, Netsu Netsu No Mi, Mochi Mochi No Mi, Atsu Atsu No Mi, Horo Horo No Mi, Shimo Shimo no Mi, Wapu Wapu no Mi, Gunyo Gunyo no Mi
-';
-$mitico1='Fuwa Fuwa No Mi, Riki Riki no Mi, Shiku Shiku no Mi, Bari Bari No Mi, Gura Gura No Mi, Nikyu Nikyu No Mi, Zushi Zushi No Mi, Doku Doku No Mi, Ope Ope No Mi, Soru Soru No Mi, Ito Ito No Mi, Kira Kira No Mi, Moa Moa No Mi, Gasha Gasha No Mi';
 
+$comum1 = 'Beri Beri no Mi, Shari Shari no Mi, Fuku Fuku no Mi, Koro Koro no Mi, Sube Sube no Mi, Iro Iro no Mi, Poke Poke no Mi, Kobu Kobu no Mi, Kilo Kilo no Mi, Guru Guru no Mi, Toge Toge no Mi, Awa Awa no Mi, Ami Ami no Mi, Iro Iro no Mi, Woshu Woshu no Mi, Sui Sui no Mi, Ori Ori no Mi, Nagi Nagi no Mi, Noko Noko no Mi, Mane Mane no Mi, Kuku Kuku no Mi, Jara Jara no Mi';
 
-#ZOAN
-$comum2='Inu Inu no Mi, Inu Inu no Mi, Inu Inu no Mi, Koara Koara no Mi, Mogu Mogu no Mi, Mushi Mushi no Mi, Mushi Mushi no Mi, Tori Tori no Mi, Uma Uma no Mi, Hito Hito no Mi, Inu Inu no Mi, Uma Uma no Mi, Kame Kame no Mi';
-$epico2='Neko Neko no Mi, Neko Neko no Mi, Tori Tori no Mi, Ushi Ushi no Mi, Ushi Ushi no Mi, Sara Sara no Mi, Zou Zou no Mi, Akuma no Mi, Hebi Hebi no Mi, Hebi Hebi no Mi, Inu Inu no Mi, Rhino Rhino no Mi';
-$lendario2='Kumo Kumo no Mi, Neko Neko no Mi, Ryu Ryu no Mi, Ryu Ryu no Mi, Ryu Ryu no Mi, Ryu Ryu no Mi, Ryu Ryu no Mi, Ryu Ryu no Mi, Zou Zou no Mi
-';
-$mitico2='Batto Batto no Mi, Hito Hito no Mi, Hito Hito no Mi, Hito Hito no Mi, Hebi Hebi no Mi, Inu Inu no Mi, Inu Inu no Mi, Tori Tori no Mi, Hito Hito no Mi, Uma Uma no Mi, Uo Uo no Mi';
+$raro1 = 'Bata Bata no Mi, Bara Bara no Mi, Bane Bane no Mi, Kama Kama no Mi, Giro Giro no Mi, Hira Hira no Mi, Suke Suke no Mi, Gocha Gocha no Mi, Mini Mini no Mi, Shibo Shibo no Mi, Beta Beta no Mi, Choki Choki no Mi, Doru Doru no Mi, Noro Noro no Mi, Kuri Kuri no Mi, Memo Memo no Mi, Nui Nui no Mi, Mato Mato no Mi, Modo Modo no Mi, Bomu Bomu no Mi, Jake Jake no Mi, Supa Supa no Mi, Goe Goe no Mi, Mosa Mosa no Mi, Sabi Sabi no Mi, Muchi Muchi no Mi, Nomi Nomi no Mi, Hiso Hiso no Mi';
 
-#Logia
-#epico
-$epico3 = 'Susu Susu no Mi, Ame Ame No Mi, Pasa Pasa No Mi, Numa Numa No Mi, Yuki Yuki No Mi, Toro Toro No Mi
-';
-$lendario3='Moku Moku No Mi, Mera Mera No Mi, Suna Suna No Mi, Mori Mori no Mi
-';
-$mitico3='Hie Hie No Mi, Magu Magu No Mi, Yami Yami No Mi, Pika Pika No Mi, Goro Goro No Mi, Gasu Gasu No Mi';
+$epico1 = 'Doa Doa no Mi, Buki Buki no Mi, Kachi Kachi no Mi, Pamu Pamu no Mi, Chiyu Chiyu no Mi, Oto Oto no Mi, Buku Buku no Mi, Bisu Bisu no Mi, Gutsu Gutsu no Mi, Oshi Oshi no Mi, Ton Ton no Mi, Kyubu Kyubu no Mi, Ishi Ishi no Mi, Yomi Yomi no Mi, Mira Mira no Mi, Goru Goru no Mi, Hoya Hoya no Mi, Pero Pero no Mi, Shiro Shiro no Mi, Baku Baku no Mi, Gabu Gabu no Mi';
 
+$lendario1 = 'Mero Mero no Mi, Wara Wara no Mi, Ato Ato no Mi, Hana Hana no Mi, Hobi Hobi no Mi, Modo Modo no Mi, Nuke Nuke no Mi, Jiki Jiki no Mi, Maki Maki no Mi, Juku Juku no Mi, Kage Kage no Mi, Peto Peto no Mi, Netsu Netsu no Mi, Mochi Mochi no Mi, Atsu Atsu no Mi, Horo Horo no Mi, Shimo Shimo no Mi, Wapu Wapu no Mi, Gunyo Gunyo no Mi';
 
+$mitico1 = 'Fuwa Fuwa no Mi, Riki Riki no Mi, Shiku Shiku no Mi, Bari Bari no Mi, Gura Gura no Mi, Nikyu Nikyu no Mi, Zushi Zushi no Mi, Doku Doku no Mi, Ope Ope no Mi, Soru Soru no Mi, Ito Ito no Mi, Kira Kira no Mi, Moa Moa no Mi, Gasha Gasha no Mi';
+
+# ZOAN
+$comum2 = 'Inu Inu no Mi, Inu Inu no Mi, Inu Inu no Mi, Koara Koara no Mi, Mogu Mogu no Mi, Mushi Mushi no Mi, Mushi Mushi no Mi, Tori Tori no Mi, Uma Uma no Mi, Hito Hito no Mi, Inu Inu no Mi, Uma Uma no Mi, Kame Kame no Mi';
+
+$epico2 = 'Neko Neko no Mi, Neko Neko no Mi, Tori Tori no Mi, Ushi Ushi no Mi, Ushi Ushi no Mi, Sara Sara no Mi, Zou Zou no Mi, Akuma no Mi, Hebi Hebi no Mi, Hebi Hebi no Mi, Inu Inu no Mi, Rhino Rhino no Mi';
+
+$lendario2 = 'Kumo Kumo no Mi, Neko Neko no Mi, Ryu Ryu no Mi, Ryu Ryu no Mi, Ryu Ryu no Mi, Ryu Ryu no Mi, Ryu Ryu no Mi, Ryu Ryu no Mi, Zou Zou no Mi';
+
+$mitico2 = 'Batto Batto no Mi, Hito Hito no Mi, Hito Hito no Mi, Hito Hito no Mi, Hebi Hebi no Mi, Inu Inu no Mi, Inu Inu no Mi, Tori Tori no Mi, Hito Hito no Mi, Uma Uma no Mi, Uo Uo no Mi';
+
+# Logia
+# Épico
+$epico3 = 'Susu Susu no Mi, Ame Ame no Mi, Pasa Pasa no Mi, Numa Numa no Mi, Yuki Yuki no Mi, Toro Toro no Mi';
+
+$lendario3 = 'Moku Moku no Mi, Mera Mera no Mi, Suna Suna no Mi, Mori Mori no Mi';
+
+$mitico3 = 'Hie Hie no Mi, Magu Magu no Mi, Yami Yami no Mi, Pika Pika no Mi, Goro Goro no Mi, Gasu Gasu no Mi';
+
+$locks = 'Ito Ito no Mi, Nikyuu Nikyuu no Mi, Toshi Toshi no Mi, Ope Ope no Mi, Doku Doku no Mi, Gura Gura no Mi, Fuwa Fuwa no Mi, Zushi Zushi no Mi, Soru Soru no Mi, Mochi Mochi no Mi, Toki Toki no Mi, Nomi Nomi no Mi, Raki Raki no Mi, Gasha Gasha no Mi, Uta Uta no Mi, Yami Yami no Mi, Pika Pika no Mi, Magu, Goro Goro no Mi, Gasu Gasu no Mi, Nika Nika no Mi, Uo Uo no Mi';
 
 
 $comuns = implode( ', ', [$comum1, $comum2]);
@@ -42,7 +46,7 @@ $raros = explode(', ', $raros);
 $epicos = explode(', ', $epicos);
 $lendarios = explode(', ', $lendarios);
 $miticos = explode(', ', $miticos);
-
+$locks = explode(', ', $locks);
 
 $container = new Container;
 
@@ -51,5 +55,5 @@ $container->set('raro', $raros);
 $container->set('epico', $epicos);
 $container->set('lendario', $lendarios);
 $container->set('mitico', $miticos);
-
+$container->set('lock', $locks);
 return $container;
